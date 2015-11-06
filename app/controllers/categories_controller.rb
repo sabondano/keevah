10e45e4228bf63afe31e5c0a683b7deb555d522e
Sebastian Abondano <sabondano1@gmail.com>
2015-11-05 21:34:56 -0700

    Change total_entries on CategoryController #index action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,6 +5,6 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
-    @loan_requests = @category.loan_requests.paginate(page: params[:page], per_page: 9)
+    @loan_requests = @category.loan_requests.paginate(page: params[:page], per_page: 9, total_entries: 100)
   end
 end
