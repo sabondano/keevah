bcc05a802a5f27d2a4547325dae5dbfe414a2f1e
Sebastian Abondano <sabondano1@gmail.com>
2015-11-07 14:53:55 -0700

    Remove select form #show in BorrowersController

--- app/controllers/borrowers_controller.rb
+++ app/controllers/borrowers_controller.rb
@@ -18,8 +18,7 @@ class BorrowersController < ApplicationController
   end
 
   def show
-    @loan_requests = LoanRequest.select(:title, :description, :image_url)
-      .where(user_id: params[:id])
+    @loan_requests = LoanRequest.where(user_id: params[:id])
   end
 
   private
